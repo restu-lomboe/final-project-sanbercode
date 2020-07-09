f25613e46f1ff28fe00d4e61749250571ba03326
Kukuh KKH <Krahmadani1@gmail.com>
2020-07-09 17:55:03 +0700

    CRUD Tag

--- app/Tag.php
+++ app/Tag.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $table = "tag";
+    protected $fillable = ['nama'];
 
     public function pertanyaan() {
         return $this->belongsToMany("App\Pertanyaan");
